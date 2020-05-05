7f0a1aeb8e101286aa2faf9e4b5dc9b22ab13a91
sandy13521 <nandavarmukund1999gmail.com>
2020-05-05 21:04:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/quizgenerator/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.quizgenerator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.hide();
+    }
+
+    public void generate(View v) {
+        Intent intent = new Intent(this, Guidance.class);
+        startActivity(intent);
+    }
+
+}
