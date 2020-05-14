5309b2f9db9059e391ed08e280511c1469800473
sandy13521 <nandavarmukund1999gmail.com>
2020-05-14 18:00:54 +0530

    Fixed Bugs

--- app/src/main/java/com/example/quizgenerator/previewOfQuestion.java
+++ app/src/main/java/com/example/quizgenerator/previewOfQuestion.java
@@ -1,7 +1,10 @@
 package com.example.quizgenerator;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,7 +17,7 @@ public class previewOfQuestion extends AppCompatActivity {
 
     public LinearLayout preview;
     public TextView questionTextView;
-
+    public ArrayList<String> options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,22 +29,39 @@ public class previewOfQuestion extends AppCompatActivity {
         preview = findViewById(R.id.previewLayout);
         questionTextView = findViewById(R.id.question);
 
-        ArrayList<String> options = getIntent().getExtras().getStringArrayList("optionsAndQuestion");
+        options = getIntent().getExtras().getStringArrayList("optionsAndQuestion");
         String question = options.get(0);
-        questionTextView.setText(question);
+        questionTextView.append(question);
         questionTextView.setTextSize(20);
-        // TODO : Options not visible and add onClick method to both the buttons.
+        System.out.println(options.size());
+        // TODO : Put options to the bottom of the page
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(10, 10, 10, 10);
+        params.gravity = Gravity.BOTTOM;
+
         try {
             for (int i = 1; i < options.size(); i++) {
-                TextView option = new TextView(this);
+                TextView option = new TextView(getApplicationContext());
                 option.setText(options.get(i));
-                option.setBackgroundColor(Color.argb(0, 244, 251, 252));
+                option.setBackgroundColor(Color.argb(1, 244, 251, 252));
                 option.setId(i);
+//                option.setGravity(Gravity.BOTTOM);
+                option.setLayoutParams(params);
+                option.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+                option.setTextSize(20);
+                option.setVisibility(View.VISIBLE);
                 option.setPadding(8, 8, 8, 8);
                 preview.addView(option);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void selectOption(View v) {
+        Intent intent = new Intent(this, selectCorrectOption.class);
+        intent.putExtra("optionsAndQuestion", options);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/quizgenerator/selectCorrectOption.java
@@ -0,0 +1,14 @@
+package com.example.quizgenerator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class selectCorrectOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_correct_option);
+    }
+}
