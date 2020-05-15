e449aa383dfdf44305383449fec29898827a8c1c
sandy13521 <nandavarmukund1999gmail.com>
2020-05-15 21:53:02 +0530

    Added Selection Of Correct Option

--- app/src/main/java/com/example/quizgenerator/previewOfQuestion.java
+++ app/src/main/java/com/example/quizgenerator/previewOfQuestion.java
@@ -3,7 +3,6 @@ package com.example.quizgenerator;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -36,22 +35,22 @@ public class previewOfQuestion extends AppCompatActivity {
         System.out.println(options.size());
         // TODO : Put options to the bottom of the page
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         params.setMargins(10, 10, 10, 10);
-        params.gravity = Gravity.BOTTOM;
+
 
         try {
             for (int i = 1; i < options.size(); i++) {
                 TextView option = new TextView(getApplicationContext());
                 option.setText(options.get(i));
-                option.setBackgroundColor(Color.argb(1, 244, 251, 252));
+                option.setBackgroundColor(Color.RED);
+                option.setTextSize(20);
                 option.setId(i);
-//                option.setGravity(Gravity.BOTTOM);
                 option.setLayoutParams(params);
+                option.setPadding(8, 8, 8, 8);
                 option.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
                 option.setTextSize(20);
                 option.setVisibility(View.VISIBLE);
-                option.setPadding(8, 8, 8, 8);
                 preview.addView(option);
             }
         } catch (Exception e) {
--- app/src/main/java/com/example/quizgenerator/selectCorrectOption.java
+++ app/src/main/java/com/example/quizgenerator/selectCorrectOption.java
@@ -1,14 +1,98 @@
 package com.example.quizgenerator;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class selectCorrectOption extends AppCompatActivity {
+
+    public LinearLayout select;
+    public TextView questionTextView;
+    public ArrayList<String> options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_correct_option);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.hide();
+
+        select = findViewById(R.id.selectLayout);
+        questionTextView = findViewById(R.id.question);
+
+        options = getIntent().getExtras().getStringArrayList("optionsAndQuestion");
+        String question = options.get(0);
+        questionTextView.append(question);
+        questionTextView.setTextSize(20);
+        System.out.println(options.size());
+        // TODO : Put options to the bottom of the page
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        params.setMargins(10, 10, 10, 10);
+
+        try {
+            for (int i = 1; i < options.size(); i++) {
+                final TextView option = new TextView(getApplicationContext());
+                option.setText(options.get(i));
+                option.setBackgroundColor(Color.RED);
+                option.setId(i);
+                option.setLayoutParams(params);
+                option.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+                option.setTextSize(20);
+                option.setVisibility(View.VISIBLE);
+                option.setPadding(8, 8, 8, 8);
+                option.setOnClickListener(new View.OnClickListener() {
+                    boolean status = false;
+
+                    @Override
+                    public void onClick(View v) {
+                        if (!status) {
+                            option.setBackgroundColor(Color.GREEN);
+                            option.setSelected(true);
+                            status = true;
+                        } else {
+                            option.setBackgroundColor(Color.RED);
+                            option.setSelected(false);
+                            status = false;
+                        }
+                    }
+                });
+                select.addView(option);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addQuestion(View v) {
+        String correctOption = "None";
+        for (int i = 1; i < options.size(); i++) {
+            TextView option = findViewById(i);
+            if (option.isSelected()) {
+                correctOption = option.getText().toString();
+                break;
+            }
+        }
+        if (correctOption.equals("None")) {
+            System.out.println(correctOption);
+        } else {
+            Toast.makeText(this, "Select a Correct Option", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
+
+
+
+
+
+
