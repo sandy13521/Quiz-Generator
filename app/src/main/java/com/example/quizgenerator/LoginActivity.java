ac1ca6a3d3844dd6fa5323fcb253222570a12f5f
sandy13521 <nandavarmukund1999gmail.com>
2020-05-23 19:36:10 +0530

    Adding Quizzes

--- app/src/main/java/com/example/quizgenerator/LoginActivity.java
+++ app/src/main/java/com/example/quizgenerator/LoginActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     public Button signUpButton;
